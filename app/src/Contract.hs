a6522f7e7ac0251d3fb58d375a0f7a4c2dfd9376
Navneeth Jayendran <navneejay@gmail.com>
2021-11-06 22:12:47 -0700

    Add wallet logic for finding datum

--- app/src/Contract.hs
+++ app/src/Contract.hs
@@ -13,15 +13,19 @@
 {-# LANGUAGE StandaloneDeriving #-}
 import           Data.Text            
 import           Text.Printf
+import           Data.Map             as Map
+import           Data.Maybe           (catMaybes)
 import           Control.Monad        (void)
-import           Ledger               (Address, ScriptContext, PubKeyHash, txId)
+import           Ledger               hiding (singleton)
+import           Ledger               (Address, ScriptContext, PubKeyHash, TxOutTx, txId)
 import qualified Ledger.Constraints   as Constraints
 import qualified Ledger.Typed.Scripts as Scripts
 import           Ledger.Value         (Value)
 import           Playground.Contract
 import           Plutus.Contract
 import qualified PlutusTx
-import qualified PlutusTx.AssocMap    as Map
+import           PlutusTx             (Data (..))
+import qualified PlutusTx.AssocMap    as PlutusMap
 import           PlutusTx.Prelude     hiding (Applicative (..))
 import           qualified Prelude    as Haskell
 
@@ -33,9 +37,9 @@ PlutusTx.unstableMakeIsData ''WithdrawalAnswer
 PlutusTx.makeLift ''WithdrawalAnswer
 
 data PendingWithdrawal = PendingWithdrawal {
-    id :: Integer,
-    signatures :: [(PubKeyHash, WithdrawalAnswer)],
-    pendingPayouts :: !(Map.Map PubKeyHash Value)
+    pwID :: Integer,
+    pwSignatures :: [(PubKeyHash, WithdrawalAnswer)],
+    pwPendingPayouts :: !(PlutusMap.Map PubKeyHash Value)
 }   deriving stock (Haskell.Eq, Haskell.Show, Generic)
     deriving anyclass (FromJSON, ToJSON, ToSchema)
 
@@ -43,9 +47,10 @@ PlutusTx.unstableMakeIsData ''PendingWithdrawal
 PlutusTx.makeLift ''PendingWithdrawal
 
 data SharedBankAccountDatum = SharedBankAccountDatum {
+    datAccountID     :: Integer,
     datOwners :: ![PubKeyHash],
     datWithdrawalReqCounter :: Integer,
-    datPendingWithdrawals:: (Map.Map Integer PendingWithdrawal)
+    datPendingWithdrawals:: (PlutusMap.Map Integer PendingWithdrawal)
 }   deriving stock (Haskell.Eq, Haskell.Show, Generic)
     deriving anyclass (FromJSON, ToJSON, ToSchema)
 
@@ -53,7 +58,7 @@ PlutusTx.unstableMakeIsData ''SharedBankAccountDatum
 PlutusTx.makeLift ''SharedBankAccountDatum
 
 data WithdrawalRequest = WithdrawalRequest {
-    payouts :: !(Map.Map PubKeyHash Value)
+    wrPayouts :: !(PlutusMap.Map PubKeyHash Value)
 }   deriving stock (Haskell.Eq, Haskell.Show, Generic)
     deriving anyclass (FromJSON, ToJSON, ToSchema)
 
@@ -61,7 +66,7 @@ PlutusTx.unstableMakeIsData ''WithdrawalRequest
 PlutusTx.makeLift ''WithdrawalRequest
 
 data SharedBankingAction = RequestWithdrawal WithdrawalRequest | RespondWithdrawal Integer WithdrawalAnswer
-	deriving Show
+        deriving Show
 
 PlutusTx.unstableMakeIsData ''SharedBankingAction
 PlutusTx.makeLift ''SharedBankingAction
@@ -91,43 +96,75 @@ sharedBankAccountInstance = Scripts.mkTypedValidator @SharedBanking
         wrap = Scripts.wrapValidator @SharedBankAccountDatum @SharedBankingAction
 
 data OpenParams = OpenParams {
+    opAccountID   :: Integer,
     opInitialFund :: Value,
-    opOwners :: ![PubKeyHash]
+    opOwners :: ![PubKeyHash],
+    accountID :: !Integer
 } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
 data RequestParams = RequestParams {
-    rpRequest:: WithdrawalRequest
+    rpAccountID :: Integer,
+    rpRequest   :: WithdrawalRequest
 } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
 data RespondParams = RespondParams {
-    answer:: WithdrawalAnswer
+    rspAccountID :: Integer,
+    rspAnswer    :: WithdrawalAnswer
 } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
+validator :: Validator
+validator = Scripts.validatorScript sharedBankAccountTypedValidator
+
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
 
 -- | shared account endpoints
 
 type SharedBankAccountSchema =
-	Endpoint "open" OpenParams
+        Endpoint "open" OpenParams
     .\/ Endpoint "request" RequestParams
     .\/ Endpoint "respond" RespondParams
 
 contract :: AsContractError e => Contract () SharedBankAccountSchema e ()
 contract = selectList [open, request, respond]
 
+-- | Find the secret word in the Datum of the UTxOs
+findFirstBankAccountDatum :: Map TxOutRef ChainIndexTxOut -> Maybe SharedBankAccountDatum
+findFirstBankAccountDatum =
+    fst . Map.elems . Map.map tryGetBankAccountDatum
+    where
+        fst lst = case lst of
+            [] -> Nothing
+            (Nothing):rest -> fst rest
+            match:_ -> match 
+        tryGetBankAccountDatum o = do
+            Datum d <- either (const Nothing) Just (_ciTxOutDatum o)
+            PlutusTx.fromBuiltinData d
+
 open :: AsContractError e => Promise () SharedBankAccountSchema e ()
 open = endpoint @"open" $ \openParams -> do
-    let tx = Constraints.mustPayToTheScript (SharedBankAccountDatum (opOwners openParams) 0 Map.empty) (opInitialFund openParams) 
+    let tx = Constraints.mustPayToTheScript (SharedBankAccountDatum  (opAccountID openParams) (opOwners openParams) 0 PlutusMap.empty) (opInitialFund openParams) 
     ledgerTx <- submitTxConstraints sharedBankAccountTypedValidator tx
     void $ awaitTxConfirmed $ txId ledgerTx
     logInfo @Haskell.String $ printf "New bank account created"
 
 request :: AsContractError e => Promise () SharedBankAccountSchema e ()
-request = endpoint @"request" $ \requestParams -> do
+request = endpoint @"request" $ \RequestParams{..} -> do
     logInfo @Haskell.String $ printf "Called request endpoint"
+    utxos <- utxosAt (Scripts.validatorAddress sharedBankAccountTypedValidator)
+    let foundMatch = case (findFirstBankAccountDatum utxos) of
+            Nothing  -> False
+            (Just _) -> True
+    if foundMatch then logInfo @Haskell.String $ "found an account" else logInfo @Haskell.String $ "no such account"
 
 respond :: AsContractError e => Promise () SharedBankAccountSchema e ()
 respond = endpoint @"respond" $ \respondParams -> do 
     logInfo @Haskell.String $ printf "Called respond endpoint"
+    utxos <- utxosAt (Scripts.validatorAddress sharedBankAccountTypedValidator)
+    let foundMatch = case (findFirstBankAccountDatum utxos) of
+            Nothing  -> False
+            (Just _) -> True
+    if foundMatch then logInfo @Haskell.String $ "found an account" else logInfo @Haskell.String $ "no such account"
 
 endpoints :: AsContractError e => Contract () SharedBankAccountSchema e ()
 endpoints = contract
